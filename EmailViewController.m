f73e20802df19efdc6618b6adc2af44c17b34382
Chuck Konkol <ckonkol@gmail.com>
2013-10-13 19:36:07 -0500

    SQLITE MySugar App
    SQLITE MySugar App

---
+++ EmailViewController.m
@@ -0,0 +1,153 @@
+//
+//  EmailViewController.m
+//  MySugar
+//
+//  Created by Charles Konkol on 3/12/13.
+//  Copyright (c) 2013 RVC Student. All rights reserved.
+//
+
+#import "EmailViewController.h"
+#import "FMDatabase.h"
+//#import "FMDatabaseQueue.h"
+#import "FMResultSet.h"
+
+@implementation EmailViewController
+@synthesize emailSugar;
+@synthesize txtFullName;
+@synthesize txtDoctorEmail;
+@synthesize txtEmail;
+@synthesize scrollView=scrollView;
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    if (![emailSugar isEqual: @""])
+    {
+        NSString *Email = emailSugar;
+    self.txtEmail.text = Email;
+    }
+    
+    NSInteger id;
+    NSString *fullname;
+    NSString *email;
+	// Do any additional setup after loading the view.
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docsPath = [paths objectAtIndex:0];
+    NSString *path = [docsPath stringByAppendingPathComponent:@"MySugars.sqlite"];
+    //NSLog(@"Path: %@",path);
+    FMDatabase *database = [FMDatabase databaseWithPath:path];
+    [database open];
+	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    FMResultSet *results = [database executeQuery:@"select * from personal"];
+    while([results next]) {
+        id = [results intForColumn:@"Personalid"]  ;
+        fullname = [results stringForColumn:@"fullname"] ;
+        email = [results stringForColumn:@"email"] ;
+    }
+    NSLog(@"id: %ld",(long)id);
+    NSLog(@"FullName: %@",fullname);
+    NSLog(@"email: %@",email);
+    if (id != 1)
+    {
+        // Do any additional setup after loading the view, typically from a nib.
+        //Date to write
+        
+        //[database executeUpdate:@"insert into Personal(fullname, email) values(?,?)",
+        // @"Enter Full Name",@"Enter Email"];
+        txtDoctorEmail.text = @"Enter Full Name";
+        txtFullName.text = @"Enter Email";
+    }
+    else
+    {
+        txtDoctorEmail.text = email;
+        txtFullName.text = fullname;
+    }
+    
+    
+    // NSLog(@"Path: %int",(int)id);
+    
+    [results close]; //VERY IMPORTANT!
+    [database close];
+
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
+                                   initWithTarget:self
+                                   action:@selector(dismissKeyboard)];
+    
+    [self.view addGestureRecognizer:tap];
+
+   
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+-(void)dismissKeyboard {
+    [txtEmail resignFirstResponder];
+    [txtDoctorEmail resignFirstResponder];
+    [txtFullName resignFirstResponder];
+}
+
+- (IBAction)btnSendEmail:(id)sender {
+    [txtFullName resignFirstResponder];
+    [txtDoctorEmail resignFirstResponder];
+    [txtEmail resignFirstResponder];
+    if ([MFMailComposeViewController canSendMail])
+    {
+        // Email Subject
+        NSString *emailTitle = @"My Sugar App";
+        // Email Content
+        NSString *messageBody = txtEmail.text; // Change the message body to HTML
+        // To address
+        NSArray *toRecipents = [NSArray arrayWithObject:txtDoctorEmail.text];
+        
+        MFMailComposeViewController *mc = [[MFMailComposeViewController alloc] init];
+        mc.mailComposeDelegate = self;
+        [mc setSubject:emailTitle];
+        [mc setMessageBody:messageBody isHTML:YES];
+        [mc setToRecipients:toRecipents];
+        
+        // Present mail view controller on screen
+        [self presentViewController:mc animated:YES completion:NULL];
+    }
+    else
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Failure"
+                                                        message:@"Your device doesn't support the composer sheet"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+     
+    }
+}
+- (void) mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    switch (result)
+    {
+        case MFMailComposeResultCancelled:
+            NSLog(@"Mail cancelled");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"Mail saved");
+            break;
+        case MFMailComposeResultSent:
+            NSLog(@"Mail sent");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Mail sent failure: %@", [error localizedDescription]);
+            break;
+        default:
+            break;
+    }
+    
+    // Close the Mail Interface
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+-(IBAction) doneEditing:(id) sender {
+    [sender resignFirstResponder];
+}
+@end
---
+++ MyProfileViewController.m
@@ -0,0 +1,104 @@
+//
+//  MyProfileViewController.m
+//  MySugar
+//
+//  Created by Charles Konkol on 3/12/13.
+//  Copyright (c) 2013 RVC Student. All rights reserved.
+//
+
+#import "MyProfileViewController.h"
+#import "FMDatabase.h"
+//#import "FMDatabaseQueue.h"
+#import "FMResultSet.h"
+
+@implementation MyProfileViewController
+@synthesize txtFullName=txtFullName;
+@synthesize txtEmail=txtEmail;
+@synthesize ScrollView=ScrollView;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSInteger id;
+    NSString *fullname;
+    NSString *email;
+	// Do any additional setup after loading the view.
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docsPath = [paths objectAtIndex:0];
+    NSString *path = [docsPath stringByAppendingPathComponent:@"MySugars.sqlite"];
+    //NSLog(@"Path: %@",path);
+    FMDatabase *database = [FMDatabase databaseWithPath:path];
+    [database open];
+	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    FMResultSet *results = [database executeQuery:@"select * from personal"];
+    while([results next]) {
+        id = [results intForColumn:@"Personalid"]  ;
+        fullname = [results stringForColumn:@"fullname"] ;
+        email = [results stringForColumn:@"email"] ;
+    }
+     NSLog(@"id: %ld",(long)id);
+     NSLog(@"FullName: %@",fullname);
+     NSLog(@"email: %@",email);
+    if (id != 1)
+    {
+        // Do any additional setup after loading the view, typically from a nib.
+        //Date to write
+      
+        [database executeUpdate:@"insert into Personal(fullname, email) values(?,?)",
+         @"Enter Full Name",@"Enter Email"];
+        txtEmail.text = @"Enter Full Name";
+        txtFullName.text = @"Enter Email";
+    }
+    else
+    {
+        txtEmail.text = email;
+        txtFullName.text = fullname;
+    }
+    
+
+     // NSLog(@"Path: %int",(int)id);
+   
+    [results close]; //VERY IMPORTANT!
+    [database close];
+ 
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
+                                   initWithTarget:self
+                                   action:@selector(dismissKeyboard)];
+    
+    [self.view addGestureRecognizer:tap];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)btnSave:(id)sender {
+    [txtFullName resignFirstResponder];
+    [txtEmail resignFirstResponder];
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docsPath = [paths objectAtIndex:0];
+    NSString *path = [docsPath stringByAppendingPathComponent:@"MySugars.sqlite"];
+    NSLog(@"Path: %@",path);
+    FMDatabase *database = [FMDatabase databaseWithPath:path];
+    [database open];
+	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    [database beginTransaction];
+    NSString *query = @"";
+    query =[NSString stringWithFormat:@"UPDATE Personal SET fullname= '%@', email = '%@' WHERE Personalid = 1",txtFullName.text,txtEmail.text];
+    [database executeUpdate: query];
+    [database commit];
+    [database close];
+   
+}
+-(void)dismissKeyboard {
+    [txtFullName resignFirstResponder];
+    [txtEmail resignFirstResponder];
+}
+-(IBAction) doneEditing:(id) sender {
+    [sender resignFirstResponder];
+}
+@end
